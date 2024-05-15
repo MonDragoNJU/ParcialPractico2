51e0f5e6258219f8606a66791d54027ff229d628
Julian Mondragon <j.mondragon1@uniandes.edu.co>
2024-05-15 10:10:50 -0500

    componenteListarTerminado

---
+++ src/app/paciente/paciente-list/paciente-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Paciente } from '../paciente';
+import { PacienteService } from '../paciente.service';
+
+@Component({
+  selector: 'app-paciente-list',
+  templateUrl: './paciente-list.component.html',
+  styleUrls: ['./paciente-list.component.css']
+})
+export class PacienteListComponent implements OnInit {
+
+  pacientes: Array<Paciente> = [];
+  minors: number = 0;
+
+  constructor(private pacienteService: PacienteService) { }
+
+  getPacientes(): void {
+    this.pacienteService.getPacientes().subscribe((pacientes) => {
+      this.pacientes = pacientes;
+      this.getMinors();
+    })
+  }
+
+  getMinors(): void {
+    for (let paciente of this.pacientes){
+      if (paciente.edad < 18){
+        this.minors += 1
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.getPacientes();
+  }
+
+}
